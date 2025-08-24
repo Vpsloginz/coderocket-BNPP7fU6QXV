44ec02729547e07e05c78e966adeffd694a1b62d
Vpsloginz <justcakeplease@outlook.com>
2025-08-24 06:40:38 +0300

    Fix WebSocket HMR connection error

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
   build: {
     minify: false,
     sourcemap: false,
